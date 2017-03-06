154dfecde48ca03e5473e40e2c926a4d96a7a4cd
guptaNswati <sgupta1505@gmail.com>
2017-03-06 06:35:18 +0000

    trying to make time efficient

--- factor.sh
+++ factor.sh
@@ -1,10 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # This script factorizes numbers given in a file
 while IFS='' read -r line || [[ -n "$line" ]]; do
     factors=( $(factor $line) )
-    VAR=1
-    for((n = 2; n < ${#factors[@]}; n++)); do
-	VAR=$(( $VAR * ${factors[$n]} ))
-    done
-    printf "%s=%s*%s\n" "$line" "$VAR" "${factors[1]}"
+    printf "%s=%s*%s\n" "$line" "$(($line/${factors[1]}))" "${factors[1]}"
 done < "$1"
